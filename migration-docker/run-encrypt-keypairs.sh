c3e885964e61c69c175f6efa3ae17640134621b6
Noah Prince <chewinglass123@gmail.com>
2025-05-15 13:48:49 -0700

    Rm migration docker

--- migration-docker/run-encrypt-keypairs.sh
+++
@@ -1,6 +0,0 @@
-
-docker run -v $(pwd)/migration-docker/data:/data \
-           -it \
-           --rm \
-           --name encrypt \
-           migration:latest ./encrypt-keypairs.sh $1
--- migration-docker/run-migrate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set +e
-time ./migrate.sh
-set -e
-
-tail -f /dev/null
